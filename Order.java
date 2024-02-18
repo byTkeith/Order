3d0b6cacc9654201b211d7842f2a88044e363562
Your Name <you@example.com>
2024-02-18 13:33:33 +0200

    initialised scanner

---
+++ Order.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+
+public class Order {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter the name of the word lists text file:");
+    }
+    
+}
